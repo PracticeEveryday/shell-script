2005e03ed39350d7a3a622256473086d6cd7cf51
PracticeEveryday <developerforus@gmail.com>
2024-04-28 14:56:23 +0900

    feat: ssh-add 스크립트 develop

--- ssh-add.sh
+++ ssh-add.sh
@@ -5,39 +5,65 @@ if [ ! -d ~/.ssh ]; then
   mkdir ~/.ssh
   echo "create ~/.ssh directory"
 fi
+
 cd ~/.ssh
 
 # github email 기준 pub key 생성
 read -p "Enter your GitHub email address: " email
 read -p "Enter your public/private key filename: " filename
 
+echo "$filename 앞에 prefix로 id_rsa_가 붙습니다."
+
+prefix_filename="id_rsa_$filename"
+
 echo "✅ 이 후 콘솔의 추가 질문에 모두 enter를 입력해주세요"
 
 echo $filename | ssh-keygen -t rsa -C "$email"
-echo "https://github.com/settings/keys 접속, 로그인 후 Github 계정에 아래의 ssh 공개 키를 등록해주세요"
+echo "✅ https://github.com/settings/keys 접속, 로그인 후 Github 계정에 아래의 ssh 공개 키를 등록해주세요"
 
 cat ./$filename.pub
 
-echo "ssh 키를 등록 했다면 yes를 입력해주세요"
+echo "✅ ssh 키를 등록 했다면 yes를 입력해주세요"
 while true; do
     read input
     if [ "$input" = "yes" ]; then
-        echo "정말 ssh를 등록 하셨나요?"
+        echo "✅ 정말 ssh를 등록 하셨나요?"
             read confirmation
             if [ "$confirmation" = "yes" ]; then
-                echo "알겠습니다 :)"
+                echo "✅ 알겠습니다 :)"
                 break
             else
-                echo "다시 확인해주세요."
+                echo "✅ 다시 확인해주세요."
             fi
     else
-        echo "yes를 입력해주세요"
+        echo "✅ yes를 입력해주세요"
     fi
 done
 
-echo "ssh-add 비밀키 등록 cli 실행"
+echo "✅ ssh-add 비밀키 등록 cli 실행"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/$filename
+
+
+# git ssh config file 생성
+config_file=~/.ssh/config
+
+mkdir -p ~/.ssh/$prefix_filename
+
+# ~/.ssh/config 파일이 없으면 생성합니다.
+if [ ! -f "$config_file" ]; then
+    touch $config_file
+    echo "✅ ssh config 파일을 $config_file 경로에 생성하였습니다."
+else
+    echo "✅ $config_file 이미 존재하므로 스크립트를 계속 실행해주세요"
+fi
+
+mv $filename ./$prefix_filename
+mv $filename.pub ./$prefix_filename
+
+# config 파일에 Host 정보를 추가합니다.
+echo -e "\nHost github.com-$filename\n\tHostName github.com\n\tUser git\n\tIdentityFile ~/.ssh/$prefix_filename/$filename" >> $config_file
+echo "Host configuration added for github.com-$filename in $config_file"
 
 cd -
 
@@ -45,19 +71,18 @@ echo "🚌 가져올 레포의 SSH 링크를 입력해주세요"
 
 while true; do
     read input
-    if [ "$input" == "Done" ]; then
-        echo "다시 한 번 입력해주세요"
-            echo "알겠습니다 :)"
-            break
+    if [ "$input" == "finish" ]; then
+        echo "모든 입력을 완료했습니다. 프로그램을 종료합니다."
+        break
     else
-        echo $input Clonning...
-        git clone $input
-
-        echo "🚌 가져올 레포의 SSH 링크를 입력해주세요"
-        echo "모두다 입력 했다면 finish을 입력해 주세요"
+        # SSH 형식인지 확인하고 파일 이름을 추출합니다.
+        if [[ "$input" =~ ^git@github.com:([[:alnum:]\._-]+)/([[:alnum:]\._-]+)\.git$ ]]; then
+            filename="${BASH_REMATCH[1]}"
+            echo "🚌 Cloning $input..."
+            git clone $input
+        else
+            echo "⛔️ 유효하지 않은 SSH 링크 형식입니다. 올바른 형식으로 다시 입력해주세요."
+            echo "🔚 모두 입력했다면 'finish'을 입력해 주세요"
+        fi
     fi
 done
-
-
-echo "📝reference: https://velog.io/@sonypark/GitHubSSH%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%B4-%EC%97%AC%EB%9F%AC%EA%B0%9C%EC%9D%98-%EA%B9%83%ED%97%88%EB%B8%8C-%EA%B3%84%EC%A0%95-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0-6mk3iesh0u"
-
