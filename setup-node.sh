7760a71bb28a81c8b29a8670be50d8773f9f6a20
PracticeEveryday <developerforus@gmail.com>
2024-05-07 16:22:19 +0900

    feat: nvm 설치 스크립트

---
+++ setup-node.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+. ~/.nvm/nvm.sh
+
+read -p "다운로드 할 nvm 버전을 입력해주세요: " nvm_version
+
+nvm install $nvm_version
+
+read -p "$nvm_version 을 Default로 사용하시겠습니까?: (yes or no)" is_default
+
+if [ "$is_default" = "yes" ]; then
+  echo "🚌 default 설정 하는 중"
+  nvm alias default v$nvm_version
+fi
+
+echo "🎉 nvm 목록"
+nvm ls
