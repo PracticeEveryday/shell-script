109cdabe2bf81ae5855c96ef2ab3190ac2b2d690
PracticeEveryday <developerforus@gmail.com>
2024-05-07 16:31:40 +0900

    fix: 변수에 {} 추가

--- setup-node.sh
+++ setup-node.sh
@@ -7,11 +7,11 @@ read -p "다운로드 할 nvm 버전을 입력해주세요: " nvm_version
 
 nvm install $nvm_version
 
-read -p "$nvm_version 을 Default로 사용하시겠습니까?: (yes or no)" is_default
+read -p "${nvm_version} 을 Default로 사용하시겠습니까?: (yes or no)" is_default
 
-if [ "$is_default" = "yes" ]; then
+if [ "${is_default}" = "yes" ]; then
   echo "🚌 default 설정 하는 중"
-  nvm alias default v$nvm_version
+  nvm alias default v${nvm_version}
 fi
 
 echo "🎉 nvm 목록"
